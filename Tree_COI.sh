380c72be6de24266d305652f376b80fd4ff3c06a
bukinys <bukinys@yandex.ru>
2026-05-07 01:52:40 +0800

    Add files via upload

---
+++ Tree_COI.sh
@@ -0,0 +1,3 @@
+iqtree.exe -s Baicalia_COI.fasta -m MFP -nt AUTO -pre Baicalia_COI_model
+
+iqtree.exe -s Baicalia_COI.fasta -m HKY+F+I+G4 -bb 1000 -bnni -alrt 1000 -nt AUTO -redo -pre Baicalia_COI_ml
