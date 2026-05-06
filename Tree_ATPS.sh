42d094005dcda142a6417bc1b6258d4c8836a304
bukinys <bukinys@yandex.ru>
2026-05-07 01:54:36 +0800

    Add files via upload

---
+++ Tree_ATPS.sh
@@ -0,0 +1,3 @@
+iqtree.exe -s Baicalia_ATPSa.fasta -m MFP -nt AUTO -pre Baicalia_ATPSa_model
+
+iqtree.exe -s Baicalia_ATPSa.fasta -m HKY+F+I -bb 1000 -bnni -alrt 1000 -nt AUTO -redo -pre Baicalia_ATPSa_ml
